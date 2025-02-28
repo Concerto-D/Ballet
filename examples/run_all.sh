17a401645ad790e64d797755dfd4298aee13d902
Jolan Philippe <jolan.philippe@gmail.com>
2025-02-28 17:39:49 +0100

    Final experimental script

--- examples/run_all.sh
+++ examples/run_all.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Export Minizinc
-export MINIZINC_HOME=~/Software/MiniZincIDE-2.7.6-bundle-linux-x86_64; export PATH=$PATH:$MINIZINC_HOME/bin; minizinc --help
+export MINIZINC_HOME=~/Software/MiniZincIDE-2.7.6-bundle-linux-x86_64; export PATH=$PATH:$MINIZINC_HOME/bin
+
+mkdir results 
 
 # Run sat cases
 ./examples/test_central_user.sh 15 sat single time 
@@ -9,6 +11,11 @@ export MINIZINC_HOME=~/Software/MiniZincIDE-2.7.6-bundle-linux-x86_64; export PA
 ./examples/test_linear.sh 20 sat single time 
 ./examples/test_circular.sh 20 sat single time 
 ./examples/test_stratified.sh 20 sat single time 
+
+# Git 
+git add results/ 
+git commit -m "Results for ICSME on topological sat cases"
+git push
 
 # Run unsat cases
 ./examples/test_central_user.sh 15 unsat single time 
@@ -18,6 +25,6 @@ export MINIZINC_HOME=~/Software/MiniZincIDE-2.7.6-bundle-linux-x86_64; export PA
 ./examples/test_stratified.sh 20 unsat single time 
 
 # Git 
-# git add results/ 
-# git commit -m "Results for ICSME on topological cases"
-# git push
+git add results/ 
+git commit -m "Results for ICSME on topological unsat cases"
+git push
--- examples/tests_all.sh
+++ examples/tests_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Export Minizinc
-export MINIZINC_HOME=~/Software/MiniZincIDE-2.7.6-bundle-linux-x86_64; export PATH=$PATH:$MINIZINC_HOME/bin; minizinc --help
+export MINIZINC_HOME=~/Software/MiniZincIDE-2.7.6-bundle-linux-x86_64; export PATH=$PATH:$MINIZINC_HOME/bin;
 
 # Run sat cases
 ./examples/test_central_user.sh 1 sat single time 
@@ -9,6 +9,6 @@ export MINIZINC_HOME=~/Software/MiniZincIDE-2.7.6-bundle-linux-x86_64; export PA
 ./examples/test_linear.sh 2 sat single time 
 ./examples/test_circular.sh 2 sat single time 
 ./examples/test_stratified.sh 2 sat single time 
-./examples/test_stratified.sh 2 sat single time 
+./examples/test_stratified.sh 20 sat single time 
 
 echo "DONE !"

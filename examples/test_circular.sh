26cb7d191b1bdf4cee4d6da128519b1f7c32b01b
Jolan Philippe <jolan.philippe@gmail.com>
2025-02-28 16:38:05 +0100

    removed python version in scripts

--- examples/test_circular.sh
+++ examples/test_circular.sh
@@ -70,11 +70,11 @@ if $single; then
     [ -f $time_file ] && rm $time_file
     touch $time_file
     for ((ite=1; ite<=1; ite++)); do
-        python3.11 run_circular_provider.py -n $n $unsat_flag -inventory inventory.json $timeflag -it $ite --verbose >> $time_file &
-        python3.11 run_circular_user.py -n $n $unsat_flag -inventory inventory.json $timeflag -it $ite --verbose >> $time_file &
+        python run_circular_provider.py -n $n $unsat_flag -inventory inventory.json $timeflag -it $ite --verbose >> $time_file &
+        python run_circular_user.py -n $n $unsat_flag -inventory inventory.json $timeflag -it $ite --verbose >> $time_file &
         if [ "$n" -ne 0 ]; then
             for ((i=0; i<$n; i++)); do
-                python3.11 run_circular_transformer.py -n $n -i $i $unsat_flag -inventory inventory.json $timeflag -it $ite --verbose >> $time_file &
+                python run_circular_transformer.py -n $n -i $i $unsat_flag -inventory inventory.json $timeflag -it $ite --verbose >> $time_file &
             done
         fi
         wait

b63b148643db385d973302064aee4f387061c8ac
Jolan Philippe <jolan.philippe@gmail.com>
2025-03-06 10:09:50 +0100

    Added sync barier for experiments

--- examples/tests_gossip/central_user/run_user.py
+++ examples/tests_gossip/central_user/run_user.py
@@ -85,8 +85,10 @@ roots=[f'provider{i}' for i in range(n)]
 
 if ctime:
     plan = gossip(node, roots, cr_init, cr_local_timed, cr_msg, cr_enrich, cr_ack, cr_final_timed, timed=True, iteration=it)
+    node.global_synchro()
 else:
     plan = gossip(node, roots, cr_init, cr_local, cr_msg, cr_enrich, cr_ack, cr_final, debug=verbose)
+    node.global_synchro()
     if plan != None and len(plan.instructions()):
         print("LOCAL PLAN:")
         for instruction in plan.instructions():

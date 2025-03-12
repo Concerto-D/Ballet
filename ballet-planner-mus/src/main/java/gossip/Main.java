1c60fa1a934a4d89bd2d91325a1e1ff43f652f85
Jolan Philippe <jolan.philippe@gmail.com>
2025-03-12 08:49:26 +0100

    set strategy for Qx

--- ballet-planner-mus/src/main/java/gossip/Main.java
+++ ballet-planner-mus/src/main/java/gossip/Main.java
@@ -6,6 +6,7 @@ import gossip.json.Model2Choco;
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solver;
 import org.chocosolver.solver.constraints.Constraint;
+import org.chocosolver.solver.search.strategy.Search;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,15 +27,16 @@ public class Main {
 
 
         Solver solver = choco_model.getSolver();
-            solver.reset();
-            List<Constraint> mus = solver.findMinimumConflictingSet(Arrays.asList(choco_model.getCstrs()));
-            @SuppressWarnings("unchecked")
-            Map<Object, List<String>> tracker = (Map<Object, List<String>>) choco_model.getHook("tracker");
-            mus.forEach(c -> {
-                if (tracker.containsKey(c)) {
-                    System.out.println(tracker.get(c));
-                }
-            });
+        solver.reset();
+        // solver.setSearch(Search.inputOrderLBSearch(choco_model.retrieveIntVars(true)));
+        List<Constraint> mus = solver.findMinimumConflictingSet(Arrays.asList(choco_model.getCstrs()));
+        @SuppressWarnings("unchecked")
+        Map<Object, List<String>> tracker = (Map<Object, List<String>>) choco_model.getHook("tracker");
+        mus.forEach(c -> {
+            if (tracker.containsKey(c)) {
+                System.out.println(tracker.get(c));
+            }
+        });
 //        }
     }
 }

1341bb9161c0ddfa4dc118a440465f5bc579164e
Jolan Philippe <jolan.philippe@gmail.com>
2025-03-12 13:06:52 +0100

    Fixed mispath to Ballet error

--- g5k_topological.py
+++ g5k_topological.py
@@ -174,15 +174,15 @@ def run_cuser(roles, ite, result_dir):
     # run SAT 
     for i in range(_COMPONENT):
         with play_on(pattern_hosts=_CUSER_PROVIDER, roles=roles, run_as=username) as p:
-            p.shell(f"{minizinc_path()}; python {project_dir}/examples/run_provider.py -n 15 -i {i} -inventory cuser_inventory.json --time -it {ite} >> {result_dir}user_sat_provider{i}.log", background=True)
+            p.shell(f"{minizinc_path()}; python {project_dir}examples/run_provider.py -n 15 -i {i} -inventory cuser_inventory.json --time -it {ite}  --port {_PORT} >> {result_dir}user_sat_provider{i}.log", background=True)
     with play_on(pattern_hosts=_CUSER_USER, roles=roles, run_as=username) as p:
-        p.shell(f"{minizinc_path()}; python {project_dir}/examples/run_user.py -n 15 -inventory cuser_inventory.json --time -it {ite} >> {result_dir}cuser_sat_user.log")
+        p.shell(f"{minizinc_path()}; python {project_dir}examples/run_user.py -n 15 -inventory cuser_inventory.json --time -it {ite}  --port {_PORT} >> {result_dir}cuser_sat_user.log")
     # run UNSAT
     for i in range(_COMPONENT):
         with play_on(pattern_hosts=_CUSER_PROVIDER, roles=roles, run_as=username) as p:
-            p.shell(f"{minizinc_path()}; python {project_dir}/examples/run_provider.py -n 15 -i {i} --unsat -inventory cuser_inventory.json --time -it {ite} >> {result_dir}cuser_unsat_provider{i}.log", background=True)
+            p.shell(f"{minizinc_path()}; python {project_dir}examples/run_provider.py -n 15 -i {i} --unsat -inventory cuser_inventory.json --time -it {ite} --port {_PORT}  >> {result_dir}cuser_unsat_provider{i}.log", background=True)
     with play_on(pattern_hosts=_CUSER_USER, roles=roles, run_as=username) as p:
-        p.shell(f"{minizinc_path()}; python {project_dir}/examples/run_user.py -n 15  --unsat -inventory cuser_inventory.json --time -it {ite} >> {result_dir}cuser_unsat_user.log")
+        p.shell(f"{minizinc_path()}; python {project_dir}examples/run_user.py -n 15  --unsat -inventory cuser_inventory.json --time -it {ite} --port {_PORT} >> {result_dir}cuser_unsat_user.log")
     # Get results
     with play_on(pattern_hosts=_CUSER_USER, roles=roles, run_as=username) as p:
         p.fetch(src=f"{result_dir}cuser_sat_user.log", dest="~")

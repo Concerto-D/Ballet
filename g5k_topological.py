7ccf31f6df9c47723e100f8de4e714db19f0d074
Jolan Philippe <jolan.philippe@gmail.com>
2025-03-12 14:13:38 +0100

    fix script

--- g5k_topological.py
+++ g5k_topological.py
@@ -15,7 +15,7 @@ minizinc = f"/home/{username}/Software/MiniZincIDE-2.7.6-bundle-linux-x86_64/bin
 def minizinc_path():
     return f"export PATH={minizinc}:$PATH"  
 
-_COMPONENT=5
+_COMPONENT=5    
 _DEFAULT_TIME = "00:02:00"
 _DEFAULT_START = "now"
 
@@ -174,13 +174,13 @@ def run_cuser(roles, ite, result_dir):
     #1 Copy right python file
     script_place = f"{project_dir}examples/tests_gossip/central_user/"
     for i in range(_COMPONENT):
-        with play_on(pattern_hosts=_CUSER_PROVIDER, roles=roles, run_as=username) as p:
+        with play_on(pattern_hosts=_CUSER_PROVIDER+str(i), roles=roles, run_as=username) as p:
             p.shell(f"cp {script_place}run_provider.py {project_dir}")
     with play_on(pattern_hosts=_CUSER_USER, roles=roles, run_as=username) as p:
         p.shell(f"cp {script_place}run_user.py {project_dir}")
     #2.1 run SAT 
     for i in range(_COMPONENT):
-        with play_on(pattern_hosts=_CUSER_PROVIDER, roles=roles, run_as=username) as p:
+        with play_on(pattern_hosts=_CUSER_PROVIDER+str(i), roles=roles, run_as=username) as p:
             p.shell(f"{minizinc_path()}; python {project_dir}run_provider.py -n 15 -i {i} -inventory cuser_inventory.json --time -it {ite}  --port {_PORT} >> {result_dir}user_sat_provider{i}.log", background=True)
     with play_on(pattern_hosts=_CUSER_USER, roles=roles, run_as=username) as p:
         p.shell(f"{minizinc_path()}; python {project_dir}run_user.py -n 15 -inventory cuser_inventory.json --time -it {ite}  --port {_PORT} >> {result_dir}cuser_sat_user.log")
@@ -192,7 +192,7 @@ def run_cuser(roles, ite, result_dir):
     #     p.shell(f"{minizinc_path()}; python {project_dir}run_user.py -n 15  --unsat -inventory cuser_inventory.json --time -it {ite} --port {_PORT} >> {result_dir}cuser_unsat_user.log")
     #3 Get results and clean
     for i in range(_COMPONENT):
-        with play_on(pattern_hosts=_CUSER_PROVIDER, roles=roles, run_as=username) as p:
+        with play_on(pattern_hosts=_CUSER_PROVIDER+str(i), roles=roles, run_as=username) as p:
             p.fetch(src=f"{result_dir}cuser_sat_provider{i}.log", dest="~")
             p.fetch(src=f"{result_dir}cuser_unsat_provider{i}.log", dest="~")
             p.shell(f"rm {project_dir}run_provider.py ")

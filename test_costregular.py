a8b4f2bda33c494968aa789a549e55a6d0865e7c
Jolan Philippe <jolan.philippe1@univ-orleans.fr>
2026-04-28 15:20:48 +0200

    updated example

--- test_costregular.py
+++ test_costregular.py
@@ -1,31 +1,47 @@
-from ballet.assembly.concertod.components.basics import provider
+# from ballet.assembly.concertod.components.basics import provider
 from koda.gossip import gossip
-from koda.cr_gossip import CostRegularNode, cr_init, cr_local, cr_msg, cr_enrich, cr_final, cr_ack
+from koda.cr_gossip import CostRegularNode, cr_init, cr_local, cr_msg, cr_enrich, cr_final, cr_ack, PortConfigConstraint
 
 import warnings
+
+from ballet.assembly.concertod.components.basics.provider import Provider
+from ballet.planner.goal import BehaviorReconfigurationGoal, StateReconfigurationGoal
+
 warnings.filterwarnings("ignore", category=SyntaxWarning)
+
+a_provider = Provider()
+a_provider.set_name("provider")
 
 # Config local node
 connections = []
-active = {provider: 'running'}
-goals = {}
+active = {a_provider: 'running'}
+goals = {a_provider: [BehaviorReconfigurationGoal('update'), StateReconfigurationGoal("initial", final=True)]}
+
 PORT = 3000
 inventory = {
     'provider':  {'address': 'localhost', 'port_planner': PORT}
 }
 
 config_values = {
-    "test1": 0,
-    "test2": 1
+    "provider": {
+        "test1": 0,
+        "test2": 1
+    }
 }
 
+a_port_config_constraint = PortConfigConstraint("provider",
+                                                "service","test1",
+                                                "test3","==")
+
 node = CostRegularNode(id="node0",
-  admin="DevOps0", components=[provider], 
-  connections=connections,
-  active=active,
-  goals=goals,
-  port=PORT,
-  inventory=inventory)
+                       admin="DevOps0", components=[a_provider],
+                       connections=connections,
+                       active=active,
+                       goals=goals,
+                       port=PORT,
+                       inventory=inventory,
+                       config_values=config_values,
+                       port_config_constraints=[a_port_config_constraint])
 roots=['provider']
 
 plan = gossip(node, roots, cr_init, cr_local, cr_msg, cr_enrich, cr_ack, cr_final, debug=True)
